42ad671c64356ddd63c0835afc071006ad2186d6
“Erin <erinkelley27@gmail.com>
2018-08-21 13:35:00 -0400

    added things2do to server

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const parser = require('body-parser')
 const app = express()
 
 const locationController = require('./controllers/location.js')
+const things2doController = require('./controllers/location.js')
 
 app.use(parser.json())
 
 app.use('/', locationController)
+app.use('/:city', things2doController)
 
 app.listen(3001, () => console.log('Listening on port 3001 :)'))
