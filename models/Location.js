0855e2e5dde2cd9d65d7ccd605a7082ede811bea
Angelica Harrison <harrison_angelica@yahoo.com>
2018-08-22 11:38:57 -0400

    added city symbol to json and model

--- models/Location.js
+++ models/Location.js
@@ -1,33 +1,37 @@
-const mongoose = require('../db/connection')
-const Schema = mongoose.Schema
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
 
-const Things2Do = new Schema({
-  sightsee: String,
-  restaurant: String,
-  accommodation: String,
-  romanticPlace: String,
-  image: String
-},
-{ collection: 'things2do' }
-)
+const Things2Do = new Schema(
+  {
+    sightsee: String,
+    restaurant: String,
+    accommodation: String,
+    romanticPlace: String,
+    image: String
+  },
+  { collection: "things2do" }
+);
 
-const Rating = new Schema({
-  heart: Number,
-  brokenHeart: Number
-},
-{ collection: 'ratings' }
-)
+const Rating = new Schema(
+  {
+    heart: Number,
+    brokenHeart: Number
+  },
+  { collection: "ratings" }
+);
 
-const Location = new Schema({
-  city: String,
-  tagline: String,
-  image: String
-},
-{ collection: 'locations' }
-)
+const Location = new Schema(
+  {
+    symbol: String,
+    city: String,
+    tagline: String,
+    image: String
+  },
+  { collection: "locations" }
+);
 
-mongoose.model('Things2Do', Things2Do)
-mongoose.model('Rating', Rating)
-mongoose.model('Location', Location)
+mongoose.model("Things2Do", Things2Do);
+mongoose.model("Rating", Rating);
+mongoose.model("Location", Location);
 
-module.exports = mongoose
+module.exports = mongoose;
