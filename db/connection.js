d1c8c7cc35b0f775f5ab1a85a5a4249404e997d0
“Erin <erinkelley27@gmail.com>
2018-08-24 10:56:41 -0400

    added mlab database to connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV == 'production') {
+  mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})
+} else {
+  mongoose.connect('mongodb://localhost/helium', {useMongoClient: true})
+}
+
 mongoose.Promise = Promise
 
-mongoose.connect('mongodb://localhost/helium')
-  .then(connection => console.log('connected!'))
-  .catch(err => console.log('connection failed!', err))
+// mongoose.connect('mongodb://localhost/helium')
+//   .then(connection => console.log('connected!'))
+//   .catch(err => console.log('connection failed!', err))
 
 module.exports = mongoose
